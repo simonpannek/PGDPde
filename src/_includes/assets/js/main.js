7f84e91493c9ed569c498382b3f2463d96a12af9
Simon Pannek <simon.pannek@tum.de>
2020-10-26 20:26:09 +0100

    Responsive fixes and change header image to clickable svg

--- src/_includes/assets/js/main.js
+++ src/_includes/assets/js/main.js
@@ -1,19 +1,4 @@
-function setBackground(hue, sat) {
-    const style = document.documentElement.style;
-    style.setProperty("--background-hue", hue);
-    style.setProperty("--background-sat", sat + "%");
+document.getElementById("penguin").onclick = () => {
+    toggleTheme();
+    playAudio(darkTheme());
 }
-
-function updateBackground(hour = new Date().getHours(), minutes = new Date().getMinutes()) {
-    const hue = Math.floor(((hour - 6) * 60 + minutes) / 4) % 360;
-    const sat = 40 + Math.ceil(Math.sin((hour * 60 + minutes) / 1440 * Math.PI) * 60);
-
-    // Set hue and saturation from date
-    setBackground(hue, sat);
-}
-
-// Update background every 4 minutes
-setInterval(function () {
-    updateBackground();
-}, 1000 * 60 * 4);
-updateBackground();
