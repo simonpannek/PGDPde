569ca85a3db2b2edae979ad674b02d4feb7f9c64
Simon Pannek <simon.pannek@tum.de>
2020-10-28 14:35:11 +0100

    Fix colors

--- src/_includes/assets/js/beforeMain.js
+++ src/_includes/assets/js/beforeMain.js
@@ -1,19 +1,33 @@
-function setBackground(hue, sat) {
+function setBackground(hue, sat, shadowHue, shadowStrength) {
     const style = document.documentElement.style;
-    style.setProperty("--background-hue", hue);
-    style.setProperty("--background-sat", sat + "%");
+
+    style.setProperty("--upper-background-hue", hue);
+    style.setProperty("--upper-background-sat", sat + "%");
+    style.setProperty("--shadow-hue", shadowHue);
+    style.setProperty("--shadow-strength", shadowStrength);
 }
 
-function updateBackground(hour = new Date().getHours(), minutes = new Date().getMinutes()) {
-    const hue = Math.floor(((hour - 6) * 60 + minutes) / 4) % 360;
-    const sat = 40 + Math.ceil(Math.sin((hour * 60 + minutes) / 1440 * Math.PI) * 60);
+function updateBackground() {
+    // Get hue and saturation for background
+    const hue = 240 - getSin();
+    const sat = 40 + getSin();
+
+    // Get hue and strength for shadow
+    const shadowHue = getSin(40);
+    const shadowStrength = getSin(0.5, true, 4, 5);
 
     // Set hue and saturation from date
-    setBackground(hue, sat);
+    setBackground(hue, sat, shadowHue, shadowStrength);
 }
 
-// Update background every 4 minutes
+function getSin(range = 50, fixed = false, frequency = 1, shift = 0) {
+    const now = new Date();
+    const sin = Math.sin(((now.getHours() - shift) * 60 + now.getMinutes()) / 1440 * frequency * Math.PI) * range;
+    return fixed ? sin.toFixed(3) : Math.ceil(sin);
+}
+
+// Update background every 5 minutes
 setInterval(function () {
     updateBackground();
-}, 1000 * 60 * 4);
+}, 1000 * 60 * 5);
 updateBackground();
