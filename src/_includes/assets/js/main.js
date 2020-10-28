9b41e21853e7799b721a41a47fd2a1f7b0d04b79
Simon Pannek <simon.pannek@tum.de>
2020-10-28 19:40:21 +0100

    Add pins

--- src/_includes/assets/js/main.js
+++ src/_includes/assets/js/main.js
@@ -1,4 +1,43 @@
+// Access and save pins
+const updatePins = function (id) {
+    // Get pins
+    const pins = localStorage.getItem("pins") ? JSON.parse(localStorage.getItem("pins")) : [];
+
+    // Check if a pin has to be added/removed
+    if (typeof id === "string") {
+        const index = pins.indexOf(id);
+        if (index < 0) {
+            // Add pin
+            pins.push(id);
+        } else {
+            // Remove pin
+            pins.splice(index, 1);
+        }
+        // Update pins
+        localStorage.setItem("pins", JSON.stringify(pins));
+    }
+    return pins;
+};
+
+updatePins().forEach(id => {
+    const element = document.getElementById(id);
+    element.style.order = "0";
+    element.classList.toggle("pinned", true);
+});
+
+// Event listeners
 document.getElementById("penguin").onclick = () => {
     toggleTheme();
     playAudio(darkTheme());
+}
+
+const pins = document.getElementsByClassName("pin");
+for (let pin of pins) {
+    pin.onclick = event => {
+        let target = event.target;
+        while (!target.id) target = target.parentNode;
+        const pinned =  updatePins(target.id).includes(target.id);
+        target.style.order = pinned ? "0" : "1";
+        target.classList.toggle("pinned", pinned);
+    };
 }
