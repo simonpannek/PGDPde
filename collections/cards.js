87cd2553f8bac116c32630e18a25a0a84bdc42d9
Simon Pannek <simon.pannek@tum.de>
2020-11-06 21:45:02 +0100

    Fix undefined behaviour during sorting

--- collections/cards.js
+++ collections/cards.js
@@ -2,5 +2,6 @@ const getOrder = require("../modules/getOrder")
 
 module.exports = coll => {
     return coll.getFilteredByGlob("src/tutors/*.md")
-        .sort((o1, o2) => getOrder(o1.data.session) - getOrder(o2.data.session));
+        .sort((o1, o2) => getOrder(o1.data.session) - getOrder(o2.data.session)
+            || (o1.data.session > o2.data.session ? 1 : -1));
 }
